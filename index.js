a2a448bb90d4595623d18f059c98c1541376beb4
dancrtis <daniel17curtis@gmail.com>
2024-01-24 21:32:10 -0700

    feat: add ignore pages param and prefetch reqs

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 /**
  * Middleware logger for NextJS analytics to quanalytics server
  *
- * @param {Object} config - object containing request, project, and apiKey
+ * @param {Object} config - object containing request, project, apiKey, config
  * @param {Request} config.request - page request
  * @param {string} config.project - quanalytics project name
  * @param {string} config.apiKey - quanalytics api key
+ * @param {string} config.ignorePages - string of regex to ignore pages, default is 'api|_next'
  *
  * @returns {Promise<null>} - fires and forgets returning null
  */
-async function quanalytics({ request, project, apiKey }) {
+async function quanalytics({ request, project, apiKey, ignorePages = 'api|_next' }) {
   const page = request.url;
   const headers = Object.fromEntries(request.headers.entries());
 
-  if (page.includes('/_next')) {
+  const ignorePrefetch = headers['next-router-prefetch'] || headers.purpose === 'prefetch';
+  if (ignorePrefetch) {
+    return;
+  }
+
+  const ignorePagesRegex = new RegExp(ignorePages);
+  if (page.match(ignorePagesRegex)) {
     return;
   }
 
